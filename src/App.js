9e8eb478e38c257dff40b7ddec74e50339a56284
Achraf Lamhamdi <achraflamhamdi@gmail.com>
2023-01-07 15:53:35 +0100

    comment commit test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import Calendar from './pages/Calendar.jsx';
 import Login from './pages/Login';
 
+// this is a commit comment test
+
 function App() {
 
 
